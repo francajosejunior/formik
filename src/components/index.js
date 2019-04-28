a398b7e923165ea516854b11ec0326df530a8a44
francajosejunior <francielle_diniz@hotmail.com>
2019-04-28 20:13:07 -0300

    remover redux

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Button } from '@material-ui/core'
-import { increase } from '../redux/counter/counter.actions'
 
 const Counter = ({ counter, dispatch }) => {
   return (
@@ -9,17 +7,12 @@ const Counter = ({ counter, dispatch }) => {
       <Button
         variant="contained"
         color="primary"
-        onClick={() => dispatch(increase())}
       >
         Count
       </Button>
       <br />
-      {counter}
     </>
   )
 }
-const mapStateToProps = (state, ownProps) => ({
-  counter: state.counter
-})
 
-export default connect(mapStateToProps)(Counter)
+export default Counter
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { store } from '../redux'
 import Counter from './../components/index'
 
 const App = () => <Counter />
 
 export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 )

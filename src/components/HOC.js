1244b8c2c3dd7614bfdb6ddaf77c80ae9d361f35
francajosejunior <francielle_diniz@hotmail.com>
2019-04-28 22:09:28 -0300

    Higher Order Component

--- src/components/HOC.js
+++ src/components/HOC.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { withFormik } from 'formik'
+import { Paper, Grid, TextField } from '@material-ui/core'
 
 class HOC extends React.Component {
   render() {
-    return <h2>Higher Order Component</h2>
+    const { values, touched, errors, handleChange, handleBlur } = this.props
+    return (
+      <Paper>
+        <h2>Higher Order Component</h2>
+        <br />
+        <Grid container spacing={24}>
+          <Grid item>
+            <TextField
+              name="nome"
+              label="Nome"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.nome}
+            />
+          </Grid>
+        </Grid>
+        <pre>{JSON.stringify(values, 0, 2)}</pre>
+      </Paper>
+    )
   }
 }
 
-export default HOC
+export default withFormik({
+  mapPropsToValues: () => ({ nome: 'Teste' })
+})(HOC)

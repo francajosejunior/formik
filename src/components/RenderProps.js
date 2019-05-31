13a56142560a039498a6bf1b02f1161de0d418db
Junior <franca.josejunior@outlook.com>
2019-05-30 23:36:25 -0300

    palestra

--- src/components/RenderProps.js
+++ src/components/RenderProps.js
@@ -16,24 +16,12 @@ import {
 import { Formik, Field } from 'formik'
 import TextField2 from './TextField2'
 
-const initialValues = { nome: '', email: '' }
-
 class RenderProps extends React.Component {
   render() {
     return (
       <Paper>
         <h2>Render Props</h2>
         <br />
-        <Formik validateOnBlur={false}>
-          {propsFormk => {
-            return (
-              <div>
-                <input type="text" name="nome" onChange={propsFormk.onChange} />
-                <pre>{JSON.stringify(propsFormk, null, 2)}</pre>
-              </div>
-            )
-          }}
-        </Formik>
       </Paper>
     )
   }
